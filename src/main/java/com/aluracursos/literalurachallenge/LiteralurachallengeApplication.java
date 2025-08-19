5d6172ee033b792686811468612c94a8b63296e6
ElviaEchauri <echaur.ely@gmail.com>
2025-08-19 01:22:10 -0600

    configuring environment

--- src/main/java/com/aluracursos/literalurachallenge/LiteralurachallengeApplication.java
+++ src/main/java/com/aluracursos/literalurachallenge/LiteralurachallengeApplication.java
@@ -1,13 +1,18 @@
 package com.aluracursos.literalurachallenge;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LiteralurachallengeApplication {
+public class LiteralurachallengeApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LiteralurachallengeApplication.class, args);
+        SpringApplication.run(LiteralurachallengeApplication.class, args);
 	}
 
+    @Override
+    public void run(String... args) throws Exception {
+        
+    }
 }
