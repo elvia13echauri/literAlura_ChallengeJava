7a465da3d9a954d94268ed2536ad79dd94f67df4
ElviaEchauri <echaur.ely@gmail.com>
2025-08-18 22:42:06 -0600

    created the proyect

---
+++ src/main/java/com/aluracursos/literalurachallenge/LiteralurachallengeApplication.java
@@ -0,0 +1,13 @@
+package com.aluracursos.literalurachallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiteralurachallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiteralurachallengeApplication.class, args);
+	}
+
+}
